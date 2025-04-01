268d85728ae5946a373ec6992feb5c58190f2aec
mirror26 <mirrordoto@gmail.com>
2025-04-01 17:56:35 +0300

    Initial commit: Strapi project setup

---
+++ src/api/repertoire/controllers/repertoire.ts
@@ -0,0 +1,7 @@
+/**
+ * repertoire controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::repertoire.repertoire');
---
+++ src/api/repertoire/routes/repertoire.ts
@@ -0,0 +1,7 @@
+/**
+ * repertoire router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::repertoire.repertoire');
